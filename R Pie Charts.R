9319bb4b6dc5fe27aa1c2444dfed608548e68650
Shaik Ashfaq <113223950+ShaikAshfaq24056@users.noreply.github.com>
2024-03-06 12:28:29 +0530

    Add files via upload

---
+++ R Pie Charts.R
@@ -0,0 +1,10 @@
+#pie(X, Labels, Radius, Main, Col, Clockwise)  
+# Creating data for the graph.  
+x <- c(20, 65, 15, 50)  
+labels <- c("India", "America", "Shri Lanka", "Nepal")  
+# Giving the chart file a name.  
+png(file = "Country.jpg")  
+# Plotting the chart.  
+pie(x,labels)  
+# Saving the file.  
+dev.off()
